708dead1460258c72337a5c78d14cccfd622c37e
Carlos <profissional.calr@gmail.com>
2025-04-25 15:22:35 -0300

    Swapper commit

--- src/app/folder/folder.page.ts
+++ src/app/folder/folder.page.ts
@@ -1,16 +1,29 @@
-import { Component, inject, OnInit } from '@angular/core';
+import { Component, CUSTOM_ELEMENTS_SCHEMA, inject, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
-import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent} from '@ionic/angular/standalone';
+import { register } from 'swiper/element/bundle';
+
+
+register();
 
 @Component({
+  schemas: [CUSTOM_ELEMENTS_SCHEMA],
   selector: 'app-folder',
   templateUrl: './folder.page.html',
   styleUrls: ['./folder.page.scss'],
   imports: [IonHeader, IonToolbar, IonButtons, IonMenuButton, IonTitle, IonContent],
+  standalone: true,
 })
 export class FolderPage implements OnInit {
   public folder!: string;
   private activatedRoute = inject(ActivatedRoute);
+  swiperConfig = {
+    slidesPerView: 1,
+    spaceBetween: 0,
+    autoplay: {
+      delay: 1000
+    }
+  }
   constructor() {}
 
   ngOnInit() {
